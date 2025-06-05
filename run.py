0a8bddfdda4403a309aa674e081ab76d264f1687
ahmadharbi0x1337 <ahmadharbi0x1337@gmail.com>
2025-06-05 16:33:18 -0400

    C&C_T

--- run.py
+++ run.py
@@ -1,8 +1,13 @@
+"""
+Flask application runner script.
+Allows app to be started using `python run.py` or `flask run`.
+"""
+
 from app import create_app
 
+# Create the app instance
 app = create_app()
 
-
-
 if __name__ == "__main__":
+    # Only run developement server when executed directly.
     app.run(debug=True)
