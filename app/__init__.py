5a725be381a26040843157372c74b033e96c4187
ahmadharbi0x1337 <ahmadharbi0x1337@gmail.com>
2025-06-05 13:00:48 -0400

    Cleaned&Closed_Temporirly!

--- app/__init__.py
+++ app/__init__.py
@@ -1,31 +1,43 @@
+"""
+application factory and core configuration setup.
+Handles database initialization, blueprint registration, and login management.
+"""
+
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
-from .models import User, db
 
+# Initializing Extensions Without Binding.
+db = SQLAlchemy
 login_manager = LoginManager()
 bcrypt = Bcrypt()
 
 def create_app():
+    """
+    Flask application factory.
+    Creates and configures the flask app instance.    
+    """
+    
     app = Flask(__name__)
     app.config.from_object(Config)
 
+    # Bind extensions to app
     db.init_app(app)
     login_manager.init_app(app)
     bcrypt.init_app(app)
-    app.bcrypt = bcrypt
 
+    # Define login view for @login_required redirects
     login_manager.login_view = 'auth.login'
-    login_manager.login_message_category = "warning"
 
+    # Creates database tables
     with app.app_context():
         db.create_all()
 
+    # Register blueprints
     from .auth import auth_bp
     from .notes import notes_bp
-
     app.register_blueprint(auth_bp)
     app.register_blueprint(notes_bp)
 
@@ -33,4 +45,8 @@ def create_app():
 
 @login_manager.user_loader
 def load_user(user_id):
+    """
+    Loads a user by ID for Flask-Login session management.
+    """
+    from .models import User
     return User.query.get(int(user_id))
